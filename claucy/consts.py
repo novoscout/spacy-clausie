48e2de64d7b781cadece9a9c423d8eb3b8a306d3
austinjp <austin.plunkett@gmail.com>
2023-11-26 14:22:07 +0000

    More typing and general rfactoring

--- claucy/consts.py
+++ claucy/consts.py
@@ -1,21 +1,90 @@
-from typing import List
+from typing import Dict, List, Tuple
 
 __all__ : List[str] = [
-    "ClauseType",
-    "Complement"
+    "ClauseTypeSV",
+    "ClauseTypeSVA",
+    "ClauseTypeSVC",
+    "ClauseTypeSVO",
+    "ClauseTypeSVOA",
+    "ClauseTypeSVOC",
+    "ClauseTypeSVOO",
+    "ClauseTypeUndefined",
+    "ComplementBe",
+    "dictionary",
+    "verb_phrase_rules"
 ]
 
 
-class ClauseType:
-    undefined: None = None
-    SV:        str  = "SV"
-    SVA:       str  = "SVA"
-    SVC:       str  = "SVC"
-    SVO:       str  = "SVO"
-    SVOA:      str  = "SVOA"
-    SVOC:      str  = "SVOC"
-    SVOO:      str  = "SVOO"
+ClauseTypeSV:        str = "SV"
+ClauseTypeSVA:       str = "SVA"
+ClauseTypeSVC:       str = "SVC"
+ClauseTypeSVO:       str = "SVO"
+ClauseTypeSVOA:      str = "SVOA"
+ClauseTypeSVOC:      str = "SVOC"
+ClauseTypeSVOO:      str = "SVOO"
+ClauseTypeUndefined: str = "undefined"
 
 
-class Complement:
-    be: str = "be"
+ComplementBe: str = "be"
+
+
+verb_phrase_rules: List[Tuple[str,List[List[Dict[str,str]]]]] = [
+    (
+        "Auxiliary verb phrase aux-verb",
+        [[ {"POS": "AUX"}, {"POS": "VERB"} ]]
+    ),
+    (
+        "Auxiliary verb phrase",
+        [[ {"POS": "AUX"} ]]
+    ),
+    (
+        "Verb phrase",
+        [[ {"POS": "VERB"} ]]
+    )
+]
+
+dictionary: Dict[str,List[str]] = {
+    "non_ext_copular": [
+        "die", "walk"
+    ],
+    "ext_copular": [
+        "act", "appear",
+        "be", "become",
+        "come", "come out",
+        "do",
+        "end up",
+        "fall", "feel",
+        "get", "go", "grow",
+        "keep",
+        "leave", "lie", "live", "look", "love",
+        "prove",
+        "remain",
+        "seem", "smell", "sound", "stand", "stay",
+        "taste", "try", "turn", "turn up",
+        "wind up"
+    ],
+    "complex_transitive": [
+        "bring",
+        "catch",
+        "drive",
+        "get",
+        "keep",
+        "lay", "lead",
+        "place", "put",
+        "set", "show", "sit", "slip", "stand",
+        "take"
+    ],
+    "adverbs_ignore": [
+        "as",
+        "even",
+        "so",
+        "then", "thus",
+        "why"
+    ],
+    "adverbs_include": [
+        "barely",
+        "hardly",
+        "rarely",
+        "scarcely", "seldom"
+    ]
+}
